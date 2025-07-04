9a6211c46e8e378111afb62c7eba7ef9104a2a66
Lucky C. Ofili <lcofili2000@gmail.com>
2025-07-04 12:30:13 +0100

    Introduction to Typescript

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = "Favour";
+// user = 10
+console.log(user);
+var food = "Rice and Beans";
+console.log(food);
+var score = 10;
+console.log(score);
---
+++ index.ts
@@ -0,0 +1,12 @@
+let user = "Favour";
+
+// user = 10
+console.log(user);
+
+let food: string = "Rice and Beans";
+console.log(food);
+
+let score: number = 10;
+console.log(score);
+
+export {}
