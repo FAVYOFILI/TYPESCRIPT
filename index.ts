b04eafcb3c98c75c93feca4e96b9fa6322bcefd3
Lucky C. Ofili <lcofili2000@gmail.com>
2025-07-04 13:02:39 +0100

    Introduction to Typescript

--- index.ts
+++ index.ts
@@ -9,4 +9,15 @@ console.log(food);
 let score: number = 10;
 console.log(score);
 
+let Besties: string[] = ["Cherie", "Rosemary", "Lilian"];
+console.log(Besties);
+
+let scores: number[] = [19, 36, 2006];
+console.log(scores);
+
+let Outcome: [string, string, number, number, boolean] = ["Cherie", "Rosemary", 38, 65, true ];
+console.log(Outcome);
+
+
+
 export {}
