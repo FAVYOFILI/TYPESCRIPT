d1c82cf9c44db4ecbe7e80f428df0d418fe8b6ae
Lucky C. Ofili <lcofili2000@gmail.com>
2025-07-07 16:23:58 +0100

    Packages

---
+++ NODE JS/server.ts
@@ -0,0 +1,17 @@
+let correct: boolean = true;
+console.log(correct);
+
+let all: any = {
+    name: "Favour",
+    age: 30,
+    isMarried: false,
+}
+console.log(all);
+
+let person: unknown;
+person = {
+  name: "Favour",
+  age: 30,
+  isMarried: false,
+};
+console.log(person);
